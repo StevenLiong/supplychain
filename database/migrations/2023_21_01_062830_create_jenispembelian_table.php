21085a6045c528eb79342814dca6d7be3463b893
Fadli Annaufal <fadliannaufal30@gmail.com>
2023-11-23 14:08:40 +0700

    update migration

--- database/migrations/2023_21_01_062830_create_jenispembelian_table.php
+++ database/migrations/2023_21_01_062830_create_jenispembelian_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('jenispembelian', function (Blueprint $table) {
             $table->id('id_jenispembelian');
             $table->string('name_pembelian');
-            $table->integer('valuta');
         });
     }
 
--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -18,36 +18,42 @@ class SupplierSeeder extends Seeder
             'nama_supplier' => 'CHARISMA TATAMITRA ABADI, PT',
             'email' => 'charisma@gmail.com',
             'alamat' => 'JL AMPERA RAYA NO 17, GRH AMPERA LT 6, JAKARTA',
+            'valuta' => 'IDR',
         ]);
         Supplier::create([
             'kd_supplier' => 'CIP.001.00',
             'nama_supplier' => 'CV CIPTA BAHARI UTAMA',
             'email' => 'cipta@gmail.com',
             'alamat' => 'JL RAYA CIATER, BSD NO 41, TANGERANG 15416',
+            'valuta' => 'IDR',
         ]);
         Supplier::create([
             'kd_supplier' => 'CIT.001.00',
             'nama_supplier' => 'CITRA BARU PERKASA, PT',
             'email' => 'citra@gmail.com',
             'alamat' => 'JL RAYA KEBAYORAN LAMA NO 23, RT 7 RW 2 SUKABUMI SELATAN, KEBON JERUK, JAKBAR 11560',
+            'valuta' => 'IDR',
         ]);
         Supplier::create([
             'kd_supplier' => 'SUM.001.00',
             'nama_supplier' => 'CV SUMBER MAKMUR',
             'email' => 'sumber@gmail.com',
             'alamat' => 'GEDUNG METRO GLODOK, JL BLUSTRU NO 15-16-41, JAKARTA',
+            'valuta' => 'IDR',
         ]);
         Supplier::create([
             'kd_supplier' => 'SUM.002.00',
             'nama_supplier' => 'SUMBER REJEKI TEKNIK, CV',
             'email' => 'rejeki@gmail.com',
             'alamat' => 'JL. CAMAN RAYA NO.25, JATIBENING II, BEKASI 17412',
+            'valuta' => 'IDR',
         ]);
         Supplier::create([
             'kd_supplier' => 'SUM.003.00',
             'nama_supplier' => 'PT SUMBERDAYA SINAR BARU',
             'email' => 'daya@gmail.com',
             'alamat' => 'JL BANDENGAN UTARA TERUSAN 47, (JL BIDARA RAYA NO 1), JAKARTA 14450',
+            'valuta' => 'IDR',
         ]);
         
     }

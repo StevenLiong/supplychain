348a888185c0fe950320e2565d65866b8ff4ac73
Christhoper Klose <126749306+klosepanjaitan@users.noreply.github.com>
2024-01-31 13:58:44 +0700

    Update Kapasitas Produksi
    Upload kapasitas_produksi.csv & update KapasitasProduksiSeeder.php

--- database/seeders/KapasitasProduksiSeeder.php
+++ database/seeders/KapasitasProduksiSeeder.php
@@ -5,206 +5,32 @@ namespace Database\Seeders;
 use App\Models\planner\KapasitasProduksi;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KapasitasProduksiSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        $data = [
-            [
-                'tanggal' => '2024-01-01',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-02',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-03',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-04',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-05',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-06',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-07',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-08',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-09',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-10',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-11',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-12',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-13',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-14',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-15',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-16',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-17',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-18',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-19',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-20',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-21',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-22',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-23',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-24',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-25',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-26',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-27',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-28',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-29',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-30',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            [
-                'tanggal' => '2024-01-31',
-                'PL2' => '93',
-                'PL3' => '6',
-                'Drytype' => '2',
-            ],
-            
-        ];
-        
-        foreach($data as $kapasitasProduksi){
-            KapasitasProduksi::create($kapasitasProduksi);
+        $csvFile = database_path('seeders\csv\kapasitas_produksi.csv'); // Tentukan jalur ke file CSV Anda
+
+        $csvData = array_map('str_getcsv', file($csvFile));
+
+        $header = array_shift($csvData); // Mengambil header
+
+        foreach ($csvData as $row) {
+            $data = array_combine($header, $row);
+
+            DB::table('kapasitas_produksis')->insert([
+                'tanggal' => $data['tanggal'], // Sesuaikan dengan nama kolom Anda
+                'PL2' => $data['PL2'], // Sesuaikan dengan nama kolom Anda
+                'PL3' => $data['PL3'], // Sesuaikan dengan nama kolom Anda
+                'Drytype' => $data['Drytype'], // Sesuaikan dengan nama kolom Anda
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
         }
     }
 }
